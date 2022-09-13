3c6504ca8c812cc50ca2eed7e26f032beed830fb
Thabang Mmakgatla <94882887+Teiyem@users.noreply.github.com>
2022-09-13 08:08:08 +0200

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -2,87 +2,71 @@
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        if type(position) is not tuple:
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+        if (len(position) != 2) or (type(position) is not tuple)\
+                or (type(position[0]) is not int)\
+                or (type(position[1]) is not int)\
+                or (position[0] < 0) or (position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        for i in position:
-            if not isinstance(i, int) or i < 0:
-                raise TypeError("position must be a tuple of 2 positive "
-                                "integers")
-        self.__position = position
+        else:
+            self.__position = position
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for blank in range(self.position[1]):
+                print()
+            for rows in range(self.__size):
+                print(" " * self.position[0], end='')
+                print("#" * self.__size)
+
+    def __str__(self):
+        str = ""
+        if (self.__size == 0):
+            return str
+        else:
+            for blank in range(self.position[1]):
+                str += "\n"
+            for rows in range(self.__size):
+                str += (" " * self.position[0])
+                str += ("#" * self.__size)
+                if (rows != self.__size - 1):
+                    str += "\n"
+        return (str)
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        for i in value:
-            if not isinstance(i, int) or i < 0:
-                raise TypeError("position must be a tuple of \ 2 positive "
-                                "integers")
-        self.__position = value
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple:
+        if (len(value) != 2) or (type(value) is not tuple) \
+                or (type(value[0]) is not int) \
+                or (type(value[1]) is not int) \
+                or (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        for i in value:
-            if not isinstance(i, int) or i < 0:
-                raise TypeError("position must be a tuple of 2 positive "
-                                "integers")
-        self.__position = value
-
-    def area(self):
-        return int(self.__size) ** 2
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
         else:
-            for i in range(0, self.__size + self.__position[1]):
-                if i < self.__position[1]:
-                    print()
-                    continue
-                for j in range(0, self.__size + self.__position[0]):
-                    if j < self.__position[0]:
-                        print(' ', end='')
-                        continue
-                    print('#', end='')
-                print()
-
-    def __str__(self):
-        string = ''
-        if self.__size == 0:
-            return string
-        else:
-            for i in range(0, int(self.__size) + self.__position[1]):
-                if i < self.__position[1]:
-                    string += '\n'
-                    continue
-                for j in range(0, int(self.__size) + self.__position[0]):
-                    if j < self.__position[0]:
-                        string += ' '
-                        continue
-                    string += '#'
-                if i != self.__size + self.__position[1] - 1:
-                    string += '\n'
-        return string
+            self.__position = value
