66924de97eaeeba7a3988c049e956ad62da6392c
Teiyem <teethedev@gmail.com>
2022-09-22 08:39:07 -0700

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,24 +13,11 @@ def text_indentation(text):
     Text must be a string
     There should be no space at the beginning or end of each printed line
     """
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    flag1 = True
-    flag2 = False
-    for i in text:
-        if i == ' ' and flag1:
-            continue
-        else:
-            flag1 = False
-        if i == '.' or i == '?' or i == ':':
-            print('{}\n'.format(i))
-            flag2 = True
-        elif i == '\n':
-            print()
-            flag2 = True
-        else:
-            if i == ' ' and flag2:
-                continue
-            elif i != ' ' and flag2:
-                flag2 = False
-            print('{}'.format(i), end='')
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
