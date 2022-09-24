efb9c1e1489f80665991bf670f69580816a37d53
Thabang Mmakgatla <94882887+Teiyem@users.noreply.github.com>
2022-09-24 16:50:54 +0200

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+""" Create Class for square and add Exeception to size """
+
 
 class Square:
+    """ Class with Init method and Exeception try / execpt """
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """ initialization """
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        return int(self.__size) ** 2
+        """Area calculation"""
+
+        return self.__size ** 2
